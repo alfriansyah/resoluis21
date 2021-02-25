56fa4db50d227717ae47a4d0e10d8d7bbd0c57ce
alffz <alfriansyah0408@gmail.com>
2021-02-26 01:03:42 +0700

    betulin funtion

--- controller.php
+++ controller.php
@@ -7,14 +7,15 @@
     }
     
     // // default index 
-    public function index(Type $var = null){}
+    public function index(Type $var = null)
     {
       return "halaman utama";
     }
     
-    // // default tambha data 
-    public function tambah(Type $var = null){}
+    //  default tambha data 
+    public function tambah(Type $var = null)
     {
       return "tambah data diri";
     }
+  
   }
